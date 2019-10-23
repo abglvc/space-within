4dfdb8d4f3fcdaa0a3630931de199404ab641e4b
Adnan Bogilovic <adnan.bogilovic@foreo.com>
2019-10-23 13:26:21 +0200

    Initial push

---
+++ Assets/Scripts/FonePlugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FonePlugin : MonoBehaviour
+{
+    private String packageName = "com.lelo.sdk.ble.lelo_sdk_ble_f1_android_lib";
+    private AndroidJavaObject o;
+    
+    void Start()
+    {
+        Debug.Log(AndroidJNI.AttachCurrentThread());
+        o = new AndroidJavaObject(packageName+".LeloF1DeviceImpl");
+        o.Call("enableWakeUp", true);
+        o.Call("enableAccelerometerControl", true);
+        o.Call("readAccelerometerControl");
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
