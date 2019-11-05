4ff0f6355bf9a7db3d2cb2d67fc2ada69b7459e6
Adnan Bogilovic <adnan.bogilovic@foreo.com>
2019-11-05 12:56:58 +0100

    Android build inside app succeed

---
+++ Assets/Scripts/F1SLink.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class F1SLink : MonoBehaviour {
+    void Start() {
+        //AndroidJavaClass jc = new AndroidJavaClass("com.lelo.f1s.OverrideUnityActivity");
+        //AndroidJavaObject overrideActivity = jc.GetStatic<AndroidJavaObject>("instance");
+        //jc.CallStatic("UnitySendMessage", "F1SLink", "ChangeColor", "Yellow");
+        
+        //overrideActivity.Call("showMainActivity", lastStringColor);
+    }
+
+    public void ChangeColor(String a) {
+        PlayerController.sng.GetComponent<SpriteRenderer>().color = String.Compare("Yellow", a)==0 ? Color.yellow : Color.white;
+    }
+    
+    void Update() {
+        
+    }
+}
